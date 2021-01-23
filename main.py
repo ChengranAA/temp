93d29e19c644b52add74fb5be085b778308c58d6
lcraaaa <andrew_lcr@icloud.com>
2021-01-23 22:59:35 +0800

    add Chinese language support

--- main.py
+++ main.py
@@ -1,24 +1,39 @@
 from weather_get import getWeather
 import pyttsx3
 
-def main():
-
+def main(lang = "en"):
     # start speak engine, the voice is your system default
     engine = pyttsx3.init()
-    weather_list = getWeather()
+    weather_list = getWeather(lang)
 
     # create weather message
-    message = "The weather at " + str(weather_list[0]) + " is " + str(weather_list[-1]) + "." \
-    " The current temperature is " + str(weather_list[2])+ " celsius" + "." +\
-    " The current humidity is " + str(weather_list[1])+ "%" + "."
+    if lang == "en":
+        message = "The weather at " + str(weather_list[0]) + " is " + str(weather_list[-1]) + "." \
+        " The current temperature is " + str(weather_list[2])+ " celsius" + "." +\
+        " The current humidity is " + str(weather_list[1])+ "%" + "."
+
+    else:
+        message = "现在位于" + str(weather_list[0]) + "的天气是" + str(weather_list[-1]) + "。" \
+        "现在的温度是" + str(weather_list[2])+ "摄氏度" + "。" +\
+        "现在的相对湿度是" + "百分之" + str(weather_list[1])+ "。"
 
     # speak and reveal
-    engine.say("Hello Admin") # you can custume your name here
-    engine.runAndWait()
+    if lang == "en":
+        print("Hello Admin!")
+        engine.say("Hello Admin!") # you can custume your name here
+        engine.runAndWait()
+    else:
+        print("你好，管理员!")
+        engine.say("你好，管理员!")
+        engine.runAndWait()
+
     engine.say(message)
     print(message)
     engine.runAndWait()
 
     return
 
-main()
+
+# zh_cn is Chinese, en is for English
+# You have to change your system speech setting accordingly
+main(lang = "zh_cn")
--- weather_get.py
+++ weather_get.py
@@ -1,7 +1,7 @@
 import requests
 import geoip2.database
 
-def getWeather():
+def getWeather(lang="en"):
 
     # find local city name
     IP_address = requests.get('http://ifconfig.me/ip', timeout=1).text.strip()
@@ -12,15 +12,20 @@ def getWeather():
     city_name_cn = response.city.names['zh-CN']
     city_name_en = response.city.names['en']
 
-    country_iso_code = response.country.iso_code
+    country_iso_code_en = response.country.iso_code
 
-    city = city_name_en+ "," + country_iso_code
+    city = city_name_en+ "," + country_iso_code_en
 
     # get weather from openweathermap
     api_key = "c8affe8b08c4b2d249ca470d358f8925"
     base_url = "http://api.openweathermap.org/data/2.5/weather?"
 
-    complete_url = base_url + "appid=" + api_key + "&q=" + city
+
+    if lang == "en":
+        complete_url = base_url + "appid=" + api_key + "&q=" + city
+    else:
+        complete_url = base_url + "appid=" + api_key + "&q=" + city + "&lang=" + lang
+
 
     response = requests.get(complete_url)
     x = response.json()
@@ -41,4 +46,7 @@ def getWeather():
     else:
         print("Sorry, i can't find the weather of your %s" %(city_name_en))
 
-    return [city_name_en, current_humidiy, current_temperature, weather_description]
+    if lang == "en":
+        return [city_name_en, current_humidiy, current_temperature, weather_description]
+    else:
+        return [city_name_cn, current_humidiy, current_temperature, weather_description]
